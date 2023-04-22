3c68d58006901c39f422ac645e25cc7388b3b7d8
Javier Amaya <Javieramayapat@gmail.com>
2023-04-22 11:41:41 -0600

    feat[algortithms]: Add challenges for Two Pointer pattern

---
+++ arrays-strings/two-pointer-pattern/challenge-reorder-vowels.py
@@ -0,0 +1,12 @@
+"""
+Dada una cadena de caracteres, invierte solo todas las vocales de la cadena.
+Las vocales son ‘a’, ‘e’, ‘i’, ‘o’ y ‘u’, ‘A’, ‘E’, ‘I’, ‘O’, ‘U’.
+
+Ejemplo 1:
+Entrada: s = "hola"
+Salida: "halo"
+
+Ejemplo 2:
+Entrada: s = s = "leetcode"
+Salida: "leotcede"
+"""
---
+++ arrays-strings/two-pointer-pattern/challenge-validate-palindrome -with-k-replacements.py
@@ -0,0 +1,13 @@
+"""
+Dada una cadena de caracteres, averigua si la cadena dada es un palíndromo o no.
+Puedes eliminar un carácter de la cadena.Una cadena es un palíndromo si se lee igual hacia adelante y hacia atrás.
+
+Ejemplo 1:
+
+Entrada: s = "aba"
+Salida: true
+
+Ejemplo 2:
+Entrada: s = "abca"
+Salida: true
+"""
